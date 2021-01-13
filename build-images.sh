30ad0b11c2d56f6cb15b5079848449b71fc5b6b4
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-01-13 12:00:27 -0500

    Publish v0.12.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 0.11/Dockerfile -t cimg/gotham:0.11.0  -t cimg/gotham:0.11 .
-docker build --file 0.11/node/Dockerfile -t cimg/gotham:0.11.0-node  -t cimg/gotham:0.11-node .
+docker build --file 0.12/Dockerfile -t cimg/gotham:0.12.0  -t cimg/gotham:0.12 .
+docker build --file 0.12/node/Dockerfile -t cimg/gotham:0.12.0-node  -t cimg/gotham:0.12-node .
