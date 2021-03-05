67b36fca9212672ba832ef0cc52af857be3e87ec
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-03-05 16:47:56 -0500

    Publish v0.14.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 0.13/Dockerfile -t cimg/gotham:0.13.0  -t cimg/gotham:0.13 .
-docker build --file 0.13/node/Dockerfile -t cimg/gotham:0.13.0-node  -t cimg/gotham:0.13-node .
+docker build --file 0.14/Dockerfile -t cimg/strawberry:0.14.0  -t cimg/strawberry:0.14 .
+docker build --file 0.14/node/Dockerfile -t cimg/strawberry:0.14.0-node  -t cimg/strawberry:0.14-node .
