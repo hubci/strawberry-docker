4dc57312710e0c3b5e389ea08ce73f944a018a33
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-08-04 22:25:32 -0400

    Publish v0.18.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 0.17/Dockerfile -t cimg/strawberry:0.17.0  -t cimg/strawberry:0.17 .
-docker build --file 0.17/node/Dockerfile -t cimg/strawberry:0.17.0-node  -t cimg/strawberry:0.17-node .
+docker build --file 0.18/Dockerfile -t cimg/strawberry:0.18.0  -t cimg/strawberry:0.18 .
+docker build --file 0.18/node/Dockerfile -t cimg/strawberry:0.18.0-node  -t cimg/strawberry:0.18-node .
