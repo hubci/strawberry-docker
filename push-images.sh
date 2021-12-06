42d09ab9b3b8ab92b916987b79ead8aea0586e32
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-12-06 15:30:26 -0500

    Publish v0.20.1. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker push cimg/strawberry:0.20.0
+docker push cimg/strawberry:0.20.1
 docker push cimg/strawberry:0.20
-docker push cimg/strawberry:0.20.0-node
+docker push cimg/strawberry:0.20.1-node
 docker push cimg/strawberry:0.20-node
