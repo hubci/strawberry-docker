db0afa3a972935bd423cf613726f336509149dd6
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-12-05 20:12:52 -0500

    Publish v0.20.0. [release] (#39)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 0.19/Dockerfile -t cimg/strawberry:0.19.0  -t cimg/strawberry:0.19 .
-docker build --file 0.19/node/Dockerfile -t cimg/strawberry:0.19.0-node  -t cimg/strawberry:0.19-node .
+docker build --file 0.20/Dockerfile -t cimg/strawberry:0.20.0  -t cimg/strawberry:0.20 .
+docker build --file 0.20/node/Dockerfile -t cimg/strawberry:0.20.0-node  -t cimg/strawberry:0.20-node .
---
+++ push-images.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker push cimg/strawberry:0.20.0
+docker push cimg/strawberry:0.20
+docker push cimg/strawberry:0.20.0-node
+docker push cimg/strawberry:0.20-node
