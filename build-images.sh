ab2e6c1f2006e81709cb1c229f12e81291b39849
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-10-04 12:02:59 -0400

    Publish v0.23.0. [release] (#46)

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 0.22/Dockerfile -t hubci/strawberry:0.22.0  -t hubci/strawberry:0.22 .
-docker build --file 0.22/node/Dockerfile -t hubci/strawberry:0.22.0-node  -t hubci/strawberry:0.22-node .
+docker build --file 0.23/Dockerfile -t hubci/strawberry:0.23.0  -t hubci/strawberry:0.23 .
+docker build --file 0.23/node/Dockerfile -t hubci/strawberry:0.23.0-node  -t hubci/strawberry:0.23-node .
