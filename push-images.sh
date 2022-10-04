a433538a412c728a4adcb84fa7490bb64f5b1804
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-10-04 11:56:26 -0400

    Publish v0.23.0. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push hubci/strawberry:0.22.0
-docker push hubci/strawberry:0.22
-docker push hubci/strawberry:0.22.0-node
-docker push hubci/strawberry:0.22-node
+docker push hubci/strawberry:0.23.0
+docker push hubci/strawberry:0.23
+docker push hubci/strawberry:0.23.0-node
+docker push hubci/strawberry:0.23-node
